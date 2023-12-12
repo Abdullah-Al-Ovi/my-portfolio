db3057e0065f103f1936e429aeec2979bbf066d1
Noob-Ovi <abdullah.ovi098@gmail.com>
2023-12-12 18:03:35 +0600

    Added Contact Me route

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -4,65 +4,71 @@ import { Stats } from "../../Components/Stats"
 import { Skills } from "../../Components/Skills"
 import { ResumeItem } from "../../Components/ResumeItem"
 import { resume } from "../../../data"
+import { FaGithub,FaLinkedin } from "react-icons/fa"
 import CV from '../../assets/steve-Cv.pdf'
 import './about.css'
 export const About = () => {
-  return (
-    <main className="section container">
-        <section className="about">
-            <h2 className="section__title">
-                About <span>Me</span>
-            </h2>
-            <div className="about__container grid">
-                <div className="about__info">
-                    <h3 className="section__subtitle">Personal Info</h3>
-                    <ul className="info__list grid">
-                        <Info></Info>
-                    </ul>
-                    <a href={CV} download='' className="button">Download CV <span className="button_icon"><FaDownload></FaDownload></span></a>
+    return (
+        <main className="section container">
+            <section className="about">
+                <h2 className="section__title">
+                    About <span>Me</span>
+                </h2>
+                <div className="about__container grid">
+                    <div className="about__info">
+                        <h3 className="section__subtitle">Personal Info</h3>
+                        <ul className="info__list grid">
+                            <Info></Info>
+                        </ul>
+                        <div style={{margin:'13px',marginBottom:'21px',display:'flex',gap:'17px',alignItems:'center'}} className="block">
+                            <a target="_blank" rel="noreferrer" href="https://github.com/Abdullah-Al-Ovi" className="gitLinked"><FaGithub></FaGithub></a>
+                            <a target="_blank" rel="noreferrer" className="gitLinked" href="https://www.linkedin.com/in/abdullah-al-ovi-7042b12a3/" ><FaLinkedin></FaLinkedin></a>
+                        </div>
+                        <a href={CV} download='' className="button">Download CV <span className="button_icon"><FaDownload></FaDownload></span></a>
+                    </div>
+
+                    <div style={{ gridTemplateColumns: 'repeat(2, 1fr)', gap: '15px', padding: '20px', marginRight: '10px' }} className="stats">
+                        <Stats></Stats>
+                    </div>
                 </div>
+            </section>
 
-                <div  style={{ gridTemplateColumns: 'repeat(2, 1fr)',gap:'15px',padding:'20px',marginRight:'10px' }} className="stats">
-                    <Stats></Stats>
+            <div className="separator"></div>
+
+            <section className="skills">
+                <h3 className="section__subtitle subtitle__center">My Skills</h3>
+                <div className="skills_container grid">
+                    <Skills></Skills>
                 </div>
-            </div>
-        </section>
+            </section>
+            <div className="separator"></div>
 
-        <div className="separator"></div>
+            <section className="resume">
+                <h3 className="section__subtitle subtitle__center">Experience & Education</h3>
+                <div className="resume__container grid">
+                    <div className="resume__data">
+                        {
+                            resume?.map((val) => {
+                                if (val?.category === 'experience') {
+                                    return <ResumeItem key={val?.id} {...val}></ResumeItem>
+                                }
+                            })
+                        }
+                    </div>
+                    <div className="resume__data">
+                        {
+                            resume?.map((val) => {
+                                if (val?.category === 'education') {
+                                    return <ResumeItem key={val?.id} {...val}></ResumeItem>
+                                }
+                            })
+                        }
+                    </div>
 
-        <section className="skills">
-            <h3 className="section__subtitle subtitle__center">My Skills</h3>
-            <div className="skills_container grid">
-                <Skills></Skills>
-            </div>
-        </section>
-        <div className="separator"></div>
-
-        <section className="resume">
-            <h3 className="section__subtitle subtitle__center">Experience & Education</h3>
-            <div className="resume__container grid">
-                <div className="resume__data">
-                    {
-                        resume?.map((val)=>{
-                            if(val?.category==='experience'){
-                                return <ResumeItem key={val?.id} {...val}></ResumeItem>
-                            }
-                        })
-                    }
                 </div>
-                <div className="resume__data">
-                    {
-                        resume?.map((val)=>{
-                            if(val?.category==='education'){
-                                return <ResumeItem key={val?.id} {...val}></ResumeItem>
-                            }
-                        })
-                    }
-                </div>
-            </div>
-        </section>
+            </section>
 
 
-    </main>
-  )
+        </main>
+    )
 }
--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,7 +1,83 @@
-import React from 'react'
+import {useRef} from 'react'
+import { FaEnvelopeOpen, FaPhoneSquareAlt } from 'react-icons/fa'
+import { FiSend } from 'react-icons/fi'
+import './contact.css'
+import emailjs from '@emailjs/browser';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const Contact = () => {
+
+  const form = useRef()
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_1670mhw', 'template_1fa8z3t', form.current, 'Z9X3YPq5mseBUPFsV')
+      .then((result) => {
+          console.log(result.text);
+          toast.success('Your message has been sent successfully!');
+          e.target.reset()
+      }, (error) => {
+          console.log(error.text);
+          toast.error('An error occurred while sending your message. Please try again later.');
+
+      });
+      
+    }
+
   return (
-    <div>Contact</div>
+    <section className="contact container section">
+      <h2 className="section__title">Get In <span>Touch</span></h2>
+
+      <div className="contact__container container grid">
+        <div className="contact__data">
+          <h3 className="contact__title">Feel free to contact...</h3>
+
+          <p className="contact__description">
+            I&rsquo;d love to connect with you! Whether you have a project in mind, want to collaborate, or just have a question, please don&rsquo;t hesitate to reach out.
+          </p>
+
+          <div className="contact__info">
+            <div className="info__item">
+              <FaEnvelopeOpen className='info__icon'/>
+              <div>
+                <span className="info__title">Mail me: </span>
+                <h4 className="info__desc">abdullah.ovi098@gmail.com</h4>
+              </div>
+            </div>
+            <div className="info__item">
+              <FaPhoneSquareAlt className='info__icon'/>
+              <div>
+                <span className="info__title">Call me: </span>
+                <h4 className="info__desc">+880 01754448783</h4>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+        <form ref={form} onSubmit={sendEmail} className="contact_form">
+          <div className="form__input-group">
+            <div className="form__input-div">
+              <input type="text" name='user_name' placeholder='Your Name' className="form__control" required />
+            </div>
+            <div className="form__input-div">
+              <input type="email" name='user_email' placeholder='Your Email' className="form__control" required/>
+            </div>
+            <div className="form__input-div">
+              <input type="text" name='subject' placeholder='Subject' className="form__control" required/>
+            </div>
+          </div>
+
+          <div className="form__input-div">
+             <textarea name='message' placeholder='Your Message' className="form__control textarea" required></textarea>
+            </div>
+            <button type='submit' className="button">
+              Send Message
+              <span className="button__icon contact__button-icon"><FiSend></FiSend></span>
+            </button>
+        </form>
+      </div>
+      <ToastContainer />
+    </section>
   )
 }
